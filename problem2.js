9f6afa873178bf82ce2101b79acd315bfd8a5992
sakshi0078 <sj7301826@gmail.com>
2022-10-29 15:40:51 +0530

    firstcommit

---
+++ problem2.js
@@ -0,0 +1,7 @@
+let x ="sakshi";
+console.log(x);
+let y ="ram";
+console.log(y);
+let z ="sangita";
+console.log(z);
+
---
+++ problem3.js
@@ -0,0 +1,4 @@
+let x ="sakshi";
+let y =19;
+console.log(x,y);
+console.log(typeof(x,y));
